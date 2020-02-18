ddce3bf50d2cff4ec6d3155ae8dfdad05dc79837
Rachit Malik <rachit.malik@Rachits-MacBook-Pro.local>
2020-02-17 23:21:10 -0800

    Added function print item on html

--- src/code.ts
+++ src/code.ts
@@ -3,3 +3,12 @@ import {Observable} from "rxjs";
 var observable = Observable.create((observer: any) => {
     observer.next("Hey guys!")
 });
+
+observable.subscribe((x: String) => addItem(x))
+
+function addItem(val:any) {
+    var node = document.createElement("li");
+    var textnode = document.createTextNode(val);
+    node.appendChild(textnode);
+    document.getElementById("output").appendChild(node);
+}
